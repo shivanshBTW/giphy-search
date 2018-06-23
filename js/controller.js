c0147ef7c0faf1bbd773bebe2b00145ed3476eb0
Shivansh Tyagi <stshivansh99@gmail.com>
2018-06-23 20:08:38 +0530

    4
    added the default image amount and the alert message for empty input

--- js/controller.js
+++ js/controller.js
@@ -1,47 +1,52 @@
-window.addEventListener('load',init);
+window.addEventListener('load', init);
 
-function init(){
+function init() {
     bindEvents();
     fillDefault();
 }
 
-function bindEvents(){
-    document.querySelector('#searchButton').addEventListener('click',callAPI);
+function bindEvents() {
+    document.querySelector('#searchButton').addEventListener('click', callAPI);
 }
 
-function fillDefault(){
+function fillDefault() {
     document.querySelector('#searchText').value = 'cars';
     document.querySelector('#searchAmount').value = 6;
 
 }
 
-function callAPI(){
+function callAPI() {
     var search = document.querySelector('#searchText').value;
     var amount = document.querySelector('#searchAmount').value;
-    var url = "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key=ci8fx4iAngKeyEFvZMkXhXTH7JpqrcVk&limit="+amount;
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ci8fx4iAngKeyEFvZMkXhXTH7JpqrcVk&limit=" + amount;
     console.log(url);
-    fetch(url).then(received=>{
-        console.log('recieved is ',received);
-        received.json().then(data=>{
-            console.log('data is : ',data);
+    fetch(url).then(received => {
+        console.log('recieved is ', received);
+        received.json().then(data => {
+            console.log('data is : ', data);
             printImage(data.data);
-        }).catch(err=>{
-            console.log('error in data is ',err);
+        }).catch(err => {
+            console.log('error in data is ', err);
         })
-    }).catch(err=>{
+    }).catch(err => {
         console.log('err in received is ', err);
     })
 
 
 }
 
-function printImage(data){
-    document.querySelector('#imageDiv').innerHTML="";
-    for(image of data){
-        url = image.images.original.url;
-        var img = document.createElement('img');
-        img.src = url;
-        img.className = 'imageRecieved'
-        document.querySelector('#imageDiv').appendChild(img);
+function printImage(data) {
+    document.querySelector('#imageDiv').innerHTML = "";
+    if (data.length != 0) {
+        for (image of data) {
+            url = image.images.original.url;
+            var img = document.createElement('img');
+            img.src = url;
+            img.className = 'imageRecieved'
+            document.querySelector('#imageDiv').appendChild(img);
+        }
+    } else {
+        window.alert('enter something first');
     }
+
 }
